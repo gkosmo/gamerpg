0e6a848beb300aa0d7b6f96a701852abd5bfb34c
gkosmo <gkosmo1@hotmail.com>
2016-06-09 22:31:44 +0200

    index.php

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+
+        <title>Cinema MVC</title>
+        <link rel='stylesheet' type='text/css' href="bootstrap/css/bootstrap.min.css">
+        <link rel='stylesheet' type='text/css' href="css/oxo.css">
+
+        <script src="js/oxo.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+    </head>
+    <body>
+      <div class="container">
+
+        <?php
+        include('controller/controller.php');
+        ?>
+      </div>
+    </body>
+</html>
